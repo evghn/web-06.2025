a3242f9c4758e34e22989ffce2dacf6b0386d6f5
evghn <evghn94_git@mail.ru>
2025-06-06 18:56:20 +0300

    day1

---
+++ index.php
@@ -0,0 +1,146 @@
+<?php
+
+// $a = 1;
+// echo $a . "<br>";
+
+// $a = 1.1;
+// echo $a . "<br>";
+
+// // $a = "<div style=\"color: red\"> a \\ b  = $a 'hello'</div>";
+// // echo $a . "<br>";
+
+// // $b = 10;
+// // $a = '<div style="">text $b "hello"</div>';
+// // echo $a . "<br>";
+
+// // $s = "a";
+// // $s2 = "b";
+
+// // echo $s + $s2;
+
+// $a = 1;
+
+// $a = $a + 1;
+// echo $a . "<br>";
+
+// $a += 1;
+// echo $a . "<br>";
+
+// $a++;
+// echo $a . "<br>"; // 4
+
+// echo $a++ . "<br>"; // 4
+
+// echo $a . "<br>"; //5
+// $a = $a + 1;
+
+
+// echo $a . "<br>"; // 6
+// echo ++$a . "<br>"; //7
+
+// // комментарий  
+// # комментарий 
+// /*
+
+// */ 
+
+// /**
+//  * 
+//  * 
+//  */
+
+// // $user;
+// // echo $user;
+// // echo $a . "<br>";
+
+// // int (integer)
+
+// // float (double)
+
+// // bool (boolean) - true / false
+
+// // null
+
+// // array
+
+// // object (-> Class)
+
+// // string 
+
+// $a = "яблок2";
+
+// // var_dump((int)$a, (int)$a + 2); 
+
+// $a = null;
+// // var_dump((bool)$a);
+// // var_dump((int)$a);
+// // var_dump((float)$a);
+
+
+// $s = "";
+// var_dump((bool)$s);
+
+// $s = "0";
+// var_dump((bool)$s);
+
+// $i = 0;
+// var_dump((bool)$i);
+
+// $i = 0.0;
+// var_dump((bool)$i);
+
+// $m = [];
+// var_dump((bool)$m);
+
+$a = 2;
+$b = 2;
+
+$s = "app $a le";
+$s2 = "ябл ок";
+
+$userName = "Vasya";
+$user_name = "Vasya";
+// var_dump($s);
+
+$a = 0; // 1, 2, 3, 4, 5  ---
+// if (null === $bb) {
+//     echo "ok";
+// } elseif (0 === $bb ) {
+//     echo "ok2";
+// } elseif (false === $bb) {
+//     echo "ok3";
+// } else {
+//     echo "no";
+// }
+
+switch ($a) {
+    case 1:
+        echo "a = 1";
+        break;
+    case 2:
+        echo "a = 2";
+        break;
+    case 3:
+        echo "a = 3";
+        break;
+    case 4:
+        echo "a = 4";
+        break;
+    case 5:
+        echo "a = 5";
+        break;
+    default:
+        echo "a = $a";
+}
+
+
+
+
+
+
+
+
+
+
+
+
